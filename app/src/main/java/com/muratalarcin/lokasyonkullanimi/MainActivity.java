700b4f816f680db3e3900db2aa15d27e5c05a251
Murat Alarcin <murattrbl@icloud.com>
2023-11-27 19:47:48 +0300

    uses permission control

--- app/src/main/java/com/muratalarcin/lokasyonkullanimi/MainActivity.java
+++ app/src/main/java/com/muratalarcin/lokasyonkullanimi/MainActivity.java
@@ -1,14 +1,47 @@
 package com.muratalarcin.lokasyonkullanimi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.muratalarcin.lokasyonkullanimi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private int izinKontrol = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonKonum.setOnClickListener(view -> {
+            izinKontrol = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);//manifest seçerken android seç
+            if (izinKontrol != PackageManager.PERMISSION_GRANTED) {//izin onaylanırsa
+
+            }else {//onaylanmazsa
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);//burda , ile ayırıp diğer izinleri isteyebilirsin
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "İzin onaylandı", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(getApplicationContext(), "İzin reddedildi", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
