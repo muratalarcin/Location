741a7e417a7b11469f0729405d13a817c79d79ea
Murat Alarcin <murattrbl@icloud.com>
2023-11-27 20:58:48 +0300

    latest version

--- app/src/main/java/com/muratalarcin/lokasyonkullanimi/MainActivity.java
+++ app/src/main/java/com/muratalarcin/lokasyonkullanimi/MainActivity.java
@@ -8,21 +8,20 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.muratalarcin.lokasyonkullanimi.databinding.ActivityMainBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.muratalarcin.lokasyonkullanimi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private int izinKontrol = 0;
     private FusedLocationProviderClient flpc;
     private Task<Location> locationTask;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,38 +30,24 @@ public class MainActivity extends AppCompatActivity {
 
         flpc = LocationServices.getFusedLocationProviderClient(this);
 
-        binding.buttonKonum.setOnClickListener(view -> {
+        binding.buttonKonum.setOnClickListener(v -> {
             izinKontrol = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-            if (izinKontrol == PackageManager.PERMISSION_GRANTED) {
-                getLocation();
-            } else {
+            if(izinKontrol == PackageManager.PERMISSION_GRANTED){
+                locationTask = flpc.getLastLocation();
+                konumBilgisiAl();
+            }else{
                 ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
             }
         });
     }
 
-
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationTask = flpc.getLastLocation();
-        locationTask.addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    binding.textViewKonum.setText(location.getLatitude() + " - " + location.getLongitude());
-                } else {
-                    binding.textViewKonum.setText("Hata Oluştu");
-                }
+    public void konumBilgisiAl(){
+        locationTask.addOnSuccessListener(location -> {
+            if(location != null){
+                binding.textViewKonum.setText(location.getLatitude()+" - "+location.getLongitude());
+            }else{
+                binding.textViewKonum.setText("Hata Oluştu");
             }
         });
     }
@@ -70,13 +55,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 100) {
+        if(requestCode == 100){
             izinKontrol = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getApplicationContext(), "İzin onaylandı", Toast.LENGTH_SHORT).show();
-                getLocation();
-            } else {
-                Toast.makeText(getApplicationContext(), "İzin reddedildi", Toast.LENGTH_SHORT).show();
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(),"İzin onaylandı",Toast.LENGTH_SHORT).show();
+                locationTask = flpc.getLastLocation();
+                konumBilgisiAl();
+            }else{
+                Toast.makeText(getApplicationContext(),"İzin reddedildi",Toast.LENGTH_SHORT).show();
             }
         }
     }
